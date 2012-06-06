8de64e50b85b5c01608b54157251873114221b9a
Boris Tuzhilin <b.i.w@yandex.ru>
2012-06-06 18:25:10 +0400

    balans version 0.01

--- game/server/actors.py
+++ game/server/actors.py
@@ -1,10 +1,19 @@
 from game.server.bowman import NetBowman
 
 class Ranger(NetBowman):
-    pass
+    health = 350
+    axe_damage_mod = 50
+    bow_damage_mod = 38
+    spear_damage_mod = 44
 
 class Damager(NetBowman):
-    pass
+    health = 200
+    axe_damage_mod = 60
+    bow_damage_mod = 70
+    spear_damage_mod = 50
 
 class Tank(NetBowman):
-    pass
+    health = 550
+    axe_damage_mod = 70
+    bow_damage_mod = 30
+    spear_damage_mod = 36
