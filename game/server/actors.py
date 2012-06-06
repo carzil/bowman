fb564716b7490eb2690f6a56a1fe3bc16837240f
Alexander Andreev <carzil@yandex.ru>
2012-06-06 20:23:10 +0400

    fixed a lot of bugs

--- game/server/actors.py
+++ game/server/actors.py
@@ -1,19 +1,47 @@
+from random import randrange
 from game.server.bowman import NetBowman
 
 class Ranger(NetBowman):
     health = 700
-    axe_damage_mod = int(choise[x for x in range(150, 180, 5)])
-    bow_damage_mod = int(choise[x for x in range(70, 115, 5)])
-    spear_damage_mod = int(choise[x for x in range(125, 150, 5)])
+
+    @property
+    def bow_damage_mod(self):
+        return randrange(70, 115, 5)
+
+    @property
+    def spear_damage_mod(self):
+        return randrange(125, 150, 5)
+
+    @property
+    def axe_damage_mod(self):
+        return randrange(150, 180, 5)
 
 class Damager(NetBowman):
     health = 480
-    axe_damage_mod = int(choise[x for x in range(180, 230, 5)])
-    bow_damage_mod = int(choise[x for x in range(100, 135, 5)])
-    spear_damage_mod = int(choise[x for x in range(140, 165, 5)])
+
+    @property
+    def bow_damage_mod(self):
+        return randrange(100, 135, 5)
+
+    @property
+    def spear_damage_mod(self):
+        return randrange(140, 165, 5)
+
+    @property
+    def axe_damage_mod(self):
+        return randrange(180, 230, 5)
 
 class Tank(NetBowman):
     health = 1200
-    axe_damage_mod = int(choise[x for x in range(200, 250, 5)])
-    bow_damage_mod = int(choise[x for x in range(50, 80, 5)])
-    spear_damage_mod = int(choise[x for x in range(80, 115, 5)])
+
+    @property
+    def bow_damage_mod(self):
+        return randrange(50, 80, 5)
+
+    @property
+    def spear_damage_mod(self):
+        return randrange(80, 115, 5)
+
+    @property
+    def axe_damage_mod(self):
+        return randrange(200, 250, 5)
