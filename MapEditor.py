f7177ef72c7573df4e7e6384cfc2d1a90ec271de
Boris Tuzhilin <b.i.w@yandex.ru>
2012-06-06 23:43:50 +0400

    mapeditor version 0.04 half all on func

--- MapEditor.py
+++ MapEditor.py
@@ -1,17 +1,27 @@
-a,b = input("vvedite shiriny"),input("vvedite visoty")
-world_map =[[None for x in range(0,int(a))] for y in range(0,int(b))]
-biblioteca = {}
 class Matrix():
+    biblioteca = {}
     def __init__(self, make_matrix):
         self.world_map = world_map
         self.biblioteca = biblioteca
+
+    def print_map(self, a, b):
+        self.a = input("vvedite shiriny")
+        self.b = input("vvedite visoty")
+        world_map =[[None for x in range(0,int(a))] for y in range(0,int(b))]
         
     def make_matrix(self):
         out = ""
         for i in self.world_map:
-            for j in i:
-                a = input()
-                out += a + " "
+            otr = input()
+            if otr[0:2] == '>>>':
+                otrup = otr[4]
+                for j in i:
+                    out += otrup + ' '
+            out += "\n"
+            else:
+                for j in i:
+                    a = input()
+                    out += a + " "
             print("---")
             out += "\n"
         return out
