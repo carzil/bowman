f194bdbaf9d88269de1be3532b0ebeb12a66f758
Alexander Andreev <carzil@yandex.ru>
2012-06-06 10:38:47 +0400

    renamed logger 'game_log'

--- game/server/log.py
+++ game/server/log.py
@@ -4,5 +4,5 @@ LOG_FORMAT = "[%(name)s(%(levelname)s)]: %(message)s"
 logging.basicConfig(format=LOG_FORMAT)
 net_log = logging.getLogger("net")
 net_log.setLevel(logging.DEBUG)
-game_log = logging.getLogger("server")
+game_log = logging.getLogger("game")
 game_log.setLevel(logging.DEBUG)
