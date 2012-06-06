0c43241750c6957717ec3dbf8ec00895962b7c3d
Boris Tuzhilin <b.i.w@yandex.ru>
2012-06-06 19:53:07 +0400

    balans

--- game/server/actors.py
+++ game/server/actors.py
@@ -1,19 +1,19 @@
 from game.server.bowman import NetBowman
 
 class Ranger(NetBowman):
-    health = 350
-    axe_damage_mod = 50
-    bow_damage_mod = 38
-    spear_damage_mod = 44
+    health = 700
+    axe_damage_mod = int(choise[x for x in range(150, 180, 5)])
+    bow_damage_mod = int(choise[x for x in range(70, 115, 5)])
+    spear_damage_mod = int(choise[x for x in range(125, 150, 5)])
 
 class Damager(NetBowman):
-    health = 200
-    axe_damage_mod = 60
-    bow_damage_mod = 70
-    spear_damage_mod = 50
+    health = 480
+    axe_damage_mod = int(choise[x for x in range(180, 230, 5)])
+    bow_damage_mod = int(choise[x for x in range(100, 135, 5)])
+    spear_damage_mod = int(choise[x for x in range(140, 165, 5)])
 
 class Tank(NetBowman):
-    health = 550
-    axe_damage_mod = 70
-    bow_damage_mod = 30
-    spear_damage_mod = 36
+    health = 1200
+    axe_damage_mod = int(choise[x for x in range(200, 250, 5)])
+    bow_damage_mod = int(choise[x for x in range(50, 80, 5)])
+    spear_damage_mod = int(choise[x for x in range(80, 115, 5)])
