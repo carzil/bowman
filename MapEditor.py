5b371b7aca951229cfad6fd9a953b60239fc942a
Boris Tuzhilin <b.i.w@yandex.ru>
2012-06-07 21:16:21 +0400

    git add MapEditor.py! normal MapEditorgit add MapEditor.py! version 1.01

--- MapEditor.py
+++ MapEditor.py
@@ -1,35 +1,18 @@
-class Matrix():
-    biblioteca = {}
-    def __init__(self, make_matrix):
-        self.world_map = world_map
-        self.biblioteca = biblioteca
+class creat_map():
+    def __init__(self, name, a=20, b=20, sim='.'):
+        self.name = name
+        self.a = a
+        self.b = b
+        self.sim = sim
 
-    def print_map(self, a, b):
-        self.a = input("vvedite shiriny")
-        self.b = input("vvedite visoty")
-        world_map =[[None for x in range(0,int(a))] for y in range(0,int(b))]
-        
-    def make_matrix(self):
-        out = ""
-        for i in self.world_map:
-            otr = input()
-            if otr[0:2] == '>>>':
-                otrup = otr[4]
-                for j in i:
-                    out += otrup + ' '
-            out += "\n"
-            else:
-                for j in i:
-                    a = input()
-                    out += a + " "
-            print("---")
-            out += "\n"
-        return out
-
-    def biblio(self):
-        a = input()
-        biblioteca[a] = [res]
-        return biblioteca
-res = Matrix(world_map).make_matrix()
-biblioteca = Matrix(biblioteca).biblio()
-print(res, biblioteca)
+    def creat(self):
+        res_map = ''
+        world_map =[[self.sim for x in range(0,self.a)] for y in range(0,self.b)]
+        file_name = "game/server/maps/" + self.name + ".txt"
+        file = open(file_name, 'w')
+        for i in world_map:
+            for j in i:
+                res_map += j + ' '
+            file.write(res_map)
+            file.write("\n")
+            res_map = ''
