ed0791ccc3bc405b100a111d00bdbc4b95f6a6a2
Boris Tuzhilin <b.i.w@yandex.ru>
2012-06-07 23:29:47 +0400

    mapeditor version 1.03 - coment

--- MapEditor.py
+++ MapEditor.py
@@ -1,9 +1,8 @@
-print("чтобы создать карту введите: creat_map(имя карты(которую создаёш), ширина(по умолчанию = 20), высота(по умолчанию = 20), текстура(по умолчанию '.'))")
 class creat_map():
-    def __init__(self, name, sim='.', x=25, y=25):
+    def __init__(self, name, x=25, y=25, sim='.'):
         self.name = name
-        self.x = y
-        self.x = y
+        self.x = x
+        self.y = y
         self.sim = sim
 
     def creat(self):
@@ -11,7 +10,7 @@ class creat_map():
         world_map =[[self.sim for x in range(0,self.x)] for y in range(0,self.y)]
         file_name = "game/server/maps/" + self.name + ".txt"
         file = open(file_name, 'w')
-        write = str(x) + ' ' + str(y) + "\n"
+        write = str(self.x) + ' ' + str(self.y) + "\n"
         file.write(write)
         for i in world_map:
             for j in i:
@@ -19,7 +18,3 @@ class creat_map():
             file.write(res_map)
             file.write("\n")
             res_map = ''
-
-
-# чтобы создать карту введите:
-# creat_map(имя карты(которую создаёш), ширина(по умолчанию = 20), высота(по умолчанию = 20), текстура(по умолчанию '.'))
