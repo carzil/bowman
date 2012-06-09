0ac70366a5cace443693f96cc82f027c4f1512c7
Boris Tuzhilin <b.i.w@yandex.ru>
2012-06-09 23:22:01 +0400

    balans

--- bot.py
+++ bot.py
@@ -59,15 +59,21 @@ def op_helf():
             a += i
     op_health = int(a)
     return op_health
+
 class sear():
     def __init__(self, sym='+'):
         self.sym = sym
         
     def search(self):
+        a = []
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
                 if matrix[i][j] == self.sym:
-                    return True, i, j
+                a.append([i, j])
+        if a == []:
+            return Folse, 0, 0
+        else:
+            return True, a
 
 def sqrt_mi(y1, y2, x1, x2):
     if y1 >= y2 and x1 >= x2:
@@ -79,9 +85,21 @@ def sqrt_mi(y1, y2, x1, x2):
     elif y2 >= y1 and x2 >= x1:
         return round(sqrt((y2 - y1) ** 2 + (x2 - x1) ** 2))
 
-def search_health(x_u, y_u):
-    tf, y_plus, x_plus = sear('+').search()
+def take_health(x_u, y_u):
+    tf, pluss = sear('+').search()
     if tf:
+        for i in pluss:
+            i = list(map(int,i.split()))
+            x, y = i[0], i[1]
+            a = sqrt_mi(y_u, y, x_u, x)
+            sqr += [a]
+        m = min(sqr)
+        for i in sqr:
+            sem += 1
+            if i == m:
+                break
+        pluss = pluss[sem].split()
+        y_plus, x_plus = int(pluss[0]), int(pluss[1])
         if x_u > x_plus:
             res = x_u - x_plus
             return "a " + str(res)
@@ -97,19 +115,61 @@ def search_health(x_u, y_u):
     else:
         return "!"
 
+class go():
+    def __init__(self, x_u, y_u, x_op, y_op):
+        self.x_u = x_u
+        self.y_u = y_u
+        self.x_op = x_op
+        self.y_op = y_op
+        
+    def go_enemy(self):
+        if self.x_u > self.x_op:
+            res = self.x_u - self.x_op
+            return "a " + str(res)
+        elif self.x_u < self.x_op:
+            res = self.x_op - self.x_u
+            return "d " + str(res)
+        elif self.y_u > self.y_op:
+            res = self.y_u - self.y_op
+            return "w " + str(res)
+        elif self.y_u < self.y_op:
+            res = self.y_op - self.y_u
+            return "s " + str(res)
+
 def do():
     y_u, x_u = u_matrix()
     y_op, x_op = op_matrix()
     r = sqrt_mi(y_u, y_op, x_u, x_op)
     u_health, op_health = u_helf(), op_helf()
-    if u_health > 3000:
-        return "f"
-    else:
-        plus = search_health(x_u, y_u)
+    if op_health < 250:
+        if r > 15:
+            return go(x_u, y_u, x_op, x_op).go_enemy()
+        else:
+            return "f"
+    elif u_health > 800:
+        if r > 15:
+            return go(x_u, y_u, x_op, x_op).go_enemy()
+        else:
+            return "f"
+    elif u_health > op_health + 100:
+        if r > 15:
+            plus = take_health(x_u, y_u)
+            if plus != "!":
+                return plus
+            else:
+                return go(x_u, y_u, x_op, x_op).go_enemy()
+        else:
+            return "f"
+    elif u_health < op_health:
+        plus = take_health(x_u, y_u)
         if plus != "!":
             return plus
         else:
             return "f"
+    elif r < 8:
+        return "f"
+    else:
+        return go(x_u, y_u, x_op, x_op).go_enemy()
     
 def main(argv):
     global matrix
