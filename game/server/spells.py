6a34a66b69a6f5dc2ffa89f19c1c54885f81aae0
Alexander Andreev <carzil@yandex.ru>
2012-06-11 01:00:24 +0400

    newlines in spells.py

--- game/server/spells.py
+++ game/server/spells.py
@@ -1,9 +1,12 @@
 class Spell():
     mana = 0
+
     def count_damage(self, player, opponent, r):
         pass
+
 class FireBall(Spell):
     mana = 140
+    
     def count_damage(self, player, opponent, r):
         if player.mana < self.mana:
             return False, 0
