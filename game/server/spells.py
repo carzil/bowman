a6bc3c4d960d18fdc8d4a8a061edc7d6f92d113b
Alexander Andreev <carzil@yandex.ru>
2012-06-14 23:44:02 +0400

    close #30

--- game/server/spells.py
+++ game/server/spells.py
@@ -8,14 +8,10 @@ class FireBall(Spell):
     mana = 150
     
     def count_damage(self, player, opponent, r):
-        if player.mana < self.mana:
-            return False, 0
         return False, 210
 
 class HealthBreak(Spell):
     mana = 520
 
     def count_damage(self, player, opponent, r):
-        if player.mana < self.mana:
-            return False, 0
         return False, opponent.health // 4
