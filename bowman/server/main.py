3a4afbcc337cd9ddd047f4de309bd7a15331bba9
Andreev Alexander <carzil@yandex.ru>
2012-07-02 17:32:09 +0300

    server.main -> bowman.server.main

--- bowman/server/main.py
+++ bowman/server/main.py
@@ -8,6 +8,8 @@ from random import choice
 from socket import socket, error
 import select
 from threading import Thread
+from configparser import ConfigParser
+from argparse import ArgumentParser
 from .exceptions import Restart, Exit
 from .log import net_log, game_log
 from .world import World
@@ -154,3 +156,26 @@ def start(map_path, players_num, sock, itb, config):
         updater_process.start()
     except:
         raise
+
+def main():
+    game_log.info("run Bowman v1.0")
+    arg_parser = ArgumentParser(description="Bowman is a client-server console game. "
+    "See more: https://github.com/carzil/bowman")
+    arg_parser.add_argument("-c", "--config", default="config.cfg")
+    args = arg_parser.parse_args()
+    
+    config = ConfigParser(allow_no_value=True)
+    config.read(args.config)
+
+    host = config.get("general", "host", fallback="")
+    port = config.getint("general", "port", fallback=9999)
+    max_players = config.getint("game", "max_players", fallback=2)
+    itb = config.getboolean("game", "is_team_battle", fallback=False)
+    _map = config.get("game", "map")
+
+    server_socket = setup_socket(host, port)
+    while True:
+        try:
+            start(_map, max_players, server_socket, itb, config)
+        except (Exit, Restart):
+            pass
--- server.py
+++ server.py
@@ -4,34 +4,7 @@
 # This software may be used and distributed according to the terms of the
 # GNU General Public License version 2 or any later version.
 
-from configparser import ConfigParser
-from bowman.server.exceptions import Exit, Restart
-from bowman.server.log import game_log
-from bowman.server.main import start, setup_socket
-from argparse import ArgumentParser
-
-def main():
-    game_log.info("run Bowman v1.0")
-    arg_parser = ArgumentParser(description="Bowman is a client-server console game. "
-    "See more: https://github.com/carzil/bowman")
-    arg_parser.add_argument("-c", "--config", default="config.cfg")
-    args = arg_parser.parse_args()
-    
-    config = ConfigParser(allow_no_value=True)
-    config.read(args.config)
-
-    host = config.get("general", "host", fallback="")
-    port = config.getint("general", "port", fallback=9999)
-    max_players = config.getint("game", "max_players", fallback=2)
-    itb = config.getboolean("game", "is_team_battle", fallback=False)
-    _map = config.get("game", "map")
-
-    server_socket = setup_socket(host, port)
-    while True:
-        try:
-            start(_map, max_players, server_socket, itb, config)
-        except (Exit, Restart):
-            pass
+from bowman.server.main import main
 
 if __name__ == "__main__":
     main()
